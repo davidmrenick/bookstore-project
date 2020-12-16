af066bf1341efe8972a7f4bfa534c7e8cde1bfeb
David Renick <davidmrenick@gmail.com>
2020-12-16 11:46:02 -0500

    Added some rspec tests, removed unusued spec files

--- spec/models/book_review_spec.rb
+++ spec/models/book_review_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe BookReview, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    context "when rating is greater than 5" do
+        it "throws an error" do
+            author = Author.create!(first_name: "Douglas", last_name: "Adams")
+            book = Book.create!(title: "Mostly Harmless", author_id: author.id, publisher_id: 1)
+            review = BookReview.create(book_id: book.id, rating: 7)
+            review.valid?
+            expect(review.errors[:rating].first).to include("less than or equal to 5")
+        end
+    end
+    context "when rating is less than 1" do
+        it "throws an error" do
+            author = Author.create!(first_name: "Douglas", last_name: "Adams")
+            book = Book.create!(title: "Restaurant at the End of the Universe", author_id: author.id, publisher_id: 1)
+            review = BookReview.create(book_id: book.id, rating: 0)
+            review.valid?
+            expect(review.errors[:rating].first).to include("greater than or equal to 1")
+        end
+    end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,13 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-
+    context "with author name" do
+        it "returns author name in [last_name], [first_name] format" do
+            author = Author.create!(first_name: "Stephen", last_name: "Strange")
+            book = Book.create!(title: "Astronomia Nova", author_id: author.id, publisher_id: 1)
+            expect(book.author_name).to eq("Strange, Stephen")
+        end
+    end
+    context "with reviews" do
+        it "averages the review score, accurate to 1 decimal place" do
+            author = Author.create!(first_name: "Douglas", last_name: "Adams")
+            book = Book.create!(title: "So Long and Thanks for All the Fish", author_id: author.id, publisher_id: 1)
+            review1 = BookReview.create!(book_id: book.id, rating: 4)
+            review2 = BookReview.create!(book_id: book.id, rating: 5)
+            expect(book.average_rating).to eq(4.5)
+        end
+    end
+    context "with format types" do
+        it "returns all valid format types" do
+            author = Author.create!(first_name: "Christopher", last_name: "Paolini")
+            book = Book.create!(title: "", author_id: author.id, publisher_id: 1)
+            format1 = BookFormatType.create!(name: "Paperback", physical: true)
+            format2 = BookFormatType.create!(name: "Hardback", physical: true)
+            format3 = BookFormatType.create!(name: "PDF", physical: false)
+            BookFormat.create!(book_id: book.id, book_format_type_id: format1.id)
+            BookFormat.create!(book_id: book.id, book_format_type_id: format3.id)
+            expect(book.book_format_types).to include(format1, format3)
+        end
+    end
 end
 
-Rspec.describe Book, '#search' do
-	context '' do
-		it '' do
-					book = Book.search
-		end
-	end
-end
